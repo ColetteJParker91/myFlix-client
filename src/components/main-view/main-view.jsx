ba734908326dbb2e565c3261f8e712b4d477d0f0
ColetteJParker91 <103423447+ColetteJParker91@users.noreply.github.com>
2022-07-07 20:35:50 +0200

    Added main-view and movie-card
    Added new folders and files for main view and movie card

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+
+export class MainView extends React.Component {
+
+    render() {
+        const { movies } = this.props;
+
+      if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+        return (
+          <div className="main-view">
+            {movies.map(movie => <MovieCard key={movie._id} movie={movie}/>)}
+          </div>
+        );
+      }
+export default Mainview;
