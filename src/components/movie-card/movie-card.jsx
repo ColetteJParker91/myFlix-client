2cbdabe1a67d7d17c5fa8612103939c025eea96e
ColetteJParker91 <103423447+ColetteJParker91@users.noreply.github.com>
2022-07-10 19:39:52 +0200

    Update movie.card.jsx
    Attempted fix

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
 export class MovieCard extends React.Component {
-    render() {
-        const { movies, selectedMovie } = this.state;
+  
+        constructor() {
+            super();
+         this.state =  const { movies, selectedMovie } };
 
         if (selectedMovie) return <MovieView movie={selectedMovie} />;
       
         if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
-      
+            ,
         return (
           <div className="main-view">
             {movies.map(movie => <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setState({ selectedMovie: newSelectedMovie }); }} />)}
           </div>
         );
-      } }
+      }
+
+    
