c42d17ab32b7853078febfb05c022075fd64d53e
ColetteJParker91 <103423447+ColetteJParker91@users.noreply.github.com>
2022-07-16 19:18:38 +0200

    Created registration
    Registration created.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import axios from 'axios';
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { RegistrationView } from '../registration-view/registration-view';
 
-class MainView extends React.Component {
+export class MainView extends React.Component {
+
     constructor() {
         super();
         this.state = {
           movies: [],
-          selectedMovie: null
+          selectedMovie: null,
+          user: null, 
+          registered: null
         };
     }
 
@@ -40,10 +45,15 @@ class MainView extends React.Component {
     
         return (
             <div className="main-view">
-      {movies.map(movie => <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />)}
-    </div>
-            );
-      }
+        {selectedMovie
+          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
+         ))
+        }
+      </div>
+    );
+  }
 }
 
     export default MainView;
